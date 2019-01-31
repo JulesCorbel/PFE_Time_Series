f2295feb599bf8eb87dd0c5535c892c694b754bf
Jules <julescorbel.1@gmail.com>
2019-01-31 16:26:05 +0100

    Finalisation des modèles VAR avec MTS

--- Script/MTS.R
+++ Script/MTS.R
@@ -4,48 +4,47 @@ library(Matrix)
 
 appAnn<-cbind(MSEAnnStaTrain, PIBAnnStaTrain, SMICAnnStaTrain, TCHOAnnStaTrain, AGEDAnnStaTrain)
 testAnn<-cbind(MSEAnnStaTest, PIBAnnStaTest, SMICAnnStaTest, TCHOAnnStaTest, AGEDAnnStaTest)
-cor(appAnnSta)
+cor(appAnn)
 MTSplot(appAnn)
 
-#1 variable
-VARorder(appAnn[,c(1:2)], maxp=5)
-MTSAnnPIB<-VAR(appAnn[,c(1:2)], p=1, output=F, include.mean = F)
-PredAnnMTSPIB<-ts(VARpred(MTSAnnPIB, 2)$pred[,1], start=2016)
-plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSPIB), max(MSEAnnStaTest, PredAnnMTSPIB)))
-lines(PredAnnMTSPIB, col="red")
-EQM(MSEAnnStaTest, PredAnnMTSPIB)
+appTrim<-cbind(MSETrimStaTrain, PIBTrimStaTrain, SMICTrimStaTrain, TCHOTrimStaTrain)
+testTrim<-cbind(MSETrimStaTest, PIBTrimStaTest, SMICTrimStaTest, TCHOTrimStaTest)
+cor(appTrim)
+MTSplot(appTrim)
 
-VARorder(appAnn[,c(1:3)], maxp=5)
-MTSAnnSMIC<-VAR(appAnn[,c(1:3)], p=1, output=F, include.mean = T)
-PredAnnMTSSMIC<-ts(VARpred(MTSAnnSMIC, 2)$pred[,1], start=2016)
-plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSSMIC), max(MSEAnnStaTest, PredAnnMTSSMIC)))
-lines(PredAnnMTSSMIC, col="red")
-EQM(MSEAnnStaTest, PredAnnMTSSMIC)
-
-VARorder(appAnn[,c(1:4)], maxp=5)
-MTSAnnTCHO<-VAR(appAnn[,c(1:4)], p=1, output=F, include.mean = T)
-PredAnnMTSTCHO<-ts(VARpred(MTSAnnTCHO, 2)$pred[,1], start=2016)
-plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSTCHO), max(MSEAnnStaTest, PredAnnMTSTCHO)))
-lines(PredMTSTCHO, col="red")
-EQM(MSEAnnStaTest, PredAnnMTSTCHO)
-
-VARorder(appAnn[,c(1:5)], maxp=5)
-MTSAnnAGED<-VAR(appAnn[,c(1:5)], p=1, output=F, include.mean = T)
-PredAnnMTSAGED<-ts(VARpred(MTSAnnAGED, 2)$pred[,1], start=2016)
-plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSAGED), max(MSEAnnStaTest, PredAnnMTSAGED)))
-lines(PredMTSAGED, col="red")
-EQM(MSEAnnStaTest, PredAnnMTSAGED)
-
-
+#Annuelle
+  #1 variable
+  VARorder(appAnn, maxp=5)
+  MTSAnnPIB<-VAR(appAnn[,c(1:2)], p=1, output=F, include.mean = F)
+  PredAnnMTSPIB<-ts(VARpred(MTSAnnPIB, 2)$pred[,1], start=2016)
+  plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSPIB), max(MSEAnnStaTest, PredAnnMTSPIB)))
+  lines(PredAnnMTSPIB, col="red")
+  EQM(MSEAnnStaTest, PredAnnMTSPIB)
+  
+  VARorder(appAnn[,c(1:3)], maxp=5)
+  MTSAnnSMIC<-VAR(appAnn[,c(1:3)], p=1, output=F, include.mean = T)
+  PredAnnMTSSMIC<-ts(VARpred(MTSAnnSMIC, 2)$pred[,1], start=2016)
+  plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSSMIC), max(MSEAnnStaTest, PredAnnMTSSMIC)))
+  lines(PredAnnMTSSMIC, col="red")
+  EQM(MSEAnnStaTest, PredAnnMTSSMIC)
+  
+  VARorder(appAnn[,c(1:4)], maxp=5)
+  MTSAnnTCHO<-VAR(appAnn[,c(1:4)], p=1, output=F, include.mean = T)
+  PredAnnMTSTCHO<-ts(VARpred(MTSAnnTCHO, 2)$pred[,1], start=2016)
+  plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSTCHO), max(MSEAnnStaTest, PredAnnMTSTCHO)))
+  lines(PredMTSTCHO, col="red")
+  EQM(MSEAnnStaTest, PredAnnMTSTCHO)
+  
+  VARorder(appAnn[,c(1:5)], maxp=5)
+  MTSAnnAGED<-VAR(appAnn[,c(1:5)], p=1, output=F, include.mean = T)
+  PredAnnMTSAGED<-ts(VARpred(MTSAnnAGED, 2)$pred[,1], start=2016)
+  plot(MSEAnnStaTest, ylim=c(min(MSEAnnStaTest, PredAnnMTSAGED), max(MSEAnnStaTest, PredAnnMTSAGED)))
+  lines(PredMTSAGED, col="red")
+  EQM(MSEAnnStaTest, PredAnnMTSAGED)
 
 #Trimestrielle
 
   #1 variable
-    appTrim<-cbind(MSETrimStaTrain, PIBTrimStaTrain, SMICTrimStaTrain, TCHOTrimStaTrain)
-    testTrim<-cbind(MSETrimStaTest, PIBTrimStaTest, SMICTrimStaTest, TCHOTrimStaTest)
-    cor(appTrim)
-    MTSplot(appTrim)
-    
     VARorder(appTrim[,c(1:2)], maxp=10)
     MTSTrimPIB<-VAR(appTrim[,c(1:2)], p=4, output=F)
     PredTrimMTSPIB<-ts(VARpred(MTSTrimPIB, 4)$pred[,1], start=2016, frequency=4)
@@ -105,6 +104,7 @@ EQM(MSEAnnStaTest, PredAnnMTSAGED)
            legend = c('Série MSE', 'PIB & SMIC', 'PIB & Taux chômage', 'SMIC & Taux chômage'),
            col=c('black', 'red', 'blue', 'green'), lty=1, cex=0.8)
     
+    #Modèle complet
     VARorder(appTrim[,c(1:4)], maxp=20)
     MTSTrimCOMPLET<-VAR(appTrim[,c(1:4)], p=3, output=F)
     PredTrimMTSCOMPLET<-ts(VARpred(MTSTrimCOMPLET, 4)$pred[,1], start=2016, frequency=4)
